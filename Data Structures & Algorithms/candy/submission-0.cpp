71e631823824c56a4ca675ae4b449ed7b984b9c6
Utkarsh Singh <utkarshsingh200625@gmail.com>
2026-06-13 00:14:53 +0530

    Add: candy - submission-0

---
+++ Data Structures & Algorithms/candy/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int sum = 1;
+        int i = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                sum++;
+                i++;
+                continue;
+            }
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i-1]){
+                peak++;
+                sum += peak;
+                i++;
+            }
+            int down = 0;
+            while(i < n && ratings[i] < ratings[i-1]){
+                down++;
+                sum += down;
+                i++;
+            }
+            down++;
+            if(down > peak) sum += (down - peak);
+        }
+        return sum;
+    }
+};
